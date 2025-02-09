78ea818a973e97ac00b272c3e2c0209ec63e725f
Tianyi Jiang <943483630@qq.com>
2025-02-09 11:52:16 +0800

    add some books

--- code/RAG.py
+++ code/RAG.py
@@ -1,9 +1,21 @@
+import os
+import sys
+
 from langchain_community.document_loaders import PyPDFLoader
 from tqdm import tqdm
 
-loader = PyPDFLoader("./data/pdfs/1.pdf")
-doc = loader.load()
 
-for i, item in tqdm(enumerate(doc), desc="Extracting content"):
-    if i>=40 and i<=100 and item.page_content!="":
-        print(f"Page {i}: {item.page_content}")
+# open the pdf directory
+folder_path = "F:\\FastIn\\UniversityProduction\\GradutionProject\\RawData\\临床本科教材\\蓝色生死恋（字体优化版）"
+docs = os.listdir(folder_path)
+
+for doc in tqdm(docs, desc="Extracting content"):
+    bookName = doc[:-3]
+    loader = PyPDFLoader(os.path.join(folder_path, doc))
+    loaded = loader.load()
+    with open(f"./data/pdfs/{bookName}txt", "w", encoding="utf-8") as f:
+        for i, item in enumerate(loaded):
+            f.write(f"Page {i}: {item.page_content}\n")
+
+
+print("Done")
