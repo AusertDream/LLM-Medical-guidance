b1306c9a8ee762a3bf6282ef5a57ec8a3e600a62
Tianyi Jiang <943483630@qq.com>
2025-03-01 20:42:19 +0800

    delete the AAA.py code

--- code/AAA.py
+++ code/AAA.py
@@ -1,22 +1,4 @@
-import requests
-import evaluate
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import json
-import sys
-
-# 发送http请求
-
-data = {
-    "chatHistory": [],
-    "userPrompt": "I have a headache."
-}
-
-print("")
-response = requests.post("http://localhost:10000/generate", json=data)
-
-# 检查请求是否成功
-if response.status_code == 200:
-    print("请求成功！")
-    print(response.json())  # 获取响应内容
-else:
-    print(f"请求失败，状态码：{response.status_code}")
+lst = [{'role': 'system', 'content': '现在你要扮演一个医院中导诊台的护士，你的职责是根据患者的病情描述，告诉他们应该挂什么科室。如果病情描述较少，可以继续询问其他症状。要求对话尽量简短。最终必须且只给出一个科室。'}]
+dc = {'role': 'user', 'content': '你好'}
+lst.append(dc)
+print(lst)
